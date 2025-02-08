0867fa968387862ff53be87567ae0084cf04f421
swethasridhar0509 <swethasridhar.se@gmail.com>
2025-02-08 15:54:21 +0530

    added solutions pset/week/0/normals

---
+++ Week0/normals/4.sql
@@ -0,0 +1,4 @@
+--Write a SQL query to find the lowest normal ocean surface temperature.
+
+SELECT MIN("0m") AS "Lowest normal ocean surface temperature"
+FROM "normals";
---
+++ Week0/normals/5.sql
@@ -0,0 +1,4 @@
+--Write a SQL query to find the highest normal ocean surface temperature
+
+SELECT MAX("0m") AS "Highest normal ocean surface temperature"
+FROM "normals";
