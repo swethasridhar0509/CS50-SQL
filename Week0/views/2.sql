37ba84577d291c4df24aa896c34cc8819e11b30e
swethasridhar0509 <swethasridhar.se@gmail.com>
2025-02-13 22:44:11 +0530

    removed unused files

--- Week0/views/2.sql
+++
@@ -1,5 +0,0 @@
---write a SQL query to list the average colors of prints by Hokusai that include “river” in the English title. (As an aside, do they have any hint of blue?)
-
-SELECT "average_color"
-FROM "views"
-WHERE "artist" = 'Hokusai' AND "english_title" LIKE '%River%' AND SUBSTR("average_color", -2, 2) <> '00';
--- Week0/views/3.sql
+++
@@ -1,5 +0,0 @@
---write a SQL query to count how many prints by Hokusai include “Fuji” in the English title. Though all of Hokusai’s prints focused on Mt. Fuji, in how many did “Fuji” make it into the title?
-
-SELECT COUNT("id") AS count
-FROM "views"
-WHERE "artist" = 'Hokusai' AND "english_title" LIKE '%Fuji%';
