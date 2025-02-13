8eec68775697f5315b92bd2ab8a61a03d1c1a48e
swethasridhar0509 <swethasridhar.se@gmail.com>
2025-02-13 22:35:11 +0530

    removed unused files

--- Week0/players/7.sql
+++
@@ -1,5 +0,0 @@
---Write a SQL query to count the number of players who bat (or batted) right-handed and throw (or threw) left-handed, or vice versa.
-
-SELECT COUNT("id") AS count
-FROM "players"
-WHERE ("bats" = 'R' AND "throws" = 'L') OR ("bats" = 'L' AND "throws" = 'R');
